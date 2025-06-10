f4aed221c567a90b841b5333f439b0789edd26ac
SimronJ <13simran37@gmail.com>
2025-06-10 00:50:29 -0400

    ..

--- opgg/opgg.meta.js
+++ opgg/opgg.meta.js
@@ -5,5 +5,5 @@
 // @description  Calculates and displays total and average game time on OP.GG match history using a regex-based selector for the timer.
 // @author       SimronJ
 // @match        https://*.op.gg/lol/summoners*
-// @downloadURL  https://github.com/SimronJ/tempermonkeyScripts/raw/refs/heads/main/opgg/opgg.meta.js
+// @downloadURL  https://github.com/SimronJ/tempermonkeyScripts/raw/refs/heads/main/opgg/opgg.user.js
 // ==/UserScript==
