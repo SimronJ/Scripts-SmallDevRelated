cb20aa178207605731dbb57393ec50f6909fcf04
SimronJ <13simran37@gmail.com>
2025-06-17 07:14:13 -0400

    fix the meta.js

--- opgg/opgg.meta.js
+++ opgg/opgg.meta.js
@@ -5,7 +5,5 @@
 // @description  Calculates and displays total time, average time, and the most frequent champion played on OP.GG match history.
 // @author       SimronJ
 // @match        https://*.op.gg/lol/summoners*
-// @grant        none
-// @updateURL    https://github.com/SimronJ/tempermonkeyScripts/raw/refs/heads/main/opgg/opgg.meta.js
 // @downloadURL  https://github.com/SimronJ/tempermonkeyScripts/raw/refs/heads/main/opgg/opgg.user.js
 // ==/UserScript==
